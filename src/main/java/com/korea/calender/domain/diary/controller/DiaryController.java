a4b6f502540b2e4a52a0d6a6cf27103600d009f2
suvapor283 <suvapor8632@gmail.com>
2025-03-28 14:06:46 +0900

    diary list

--- src/main/java/com/korea/calender/domain/diary/controller/DiaryController.java
+++ src/main/java/com/korea/calender/domain/diary/controller/DiaryController.java
@@ -1,13 +1,29 @@
 package com.korea.calender.domain.diary.controller;
 
+import com.korea.calender.domain.diary.entity.Diary;
 import com.korea.calender.domain.diary.service.DiaryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/diary")
 public class DiaryController {
 
     private final DiaryService diaryService;
+
+    @GetMapping("/list")
+    public String list(Model model) {
+
+        List<Diary> diaryList = diaryService.getList();
+        model.addAttribute("diaryList", diaryList);
+
+        return "diary_list";
+    }
+
 }
--- src/main/java/com/korea/calender/domain/diary/service/DiaryService.java
+++ src/main/java/com/korea/calender/domain/diary/service/DiaryService.java
@@ -1,12 +1,20 @@
 package com.korea.calender.domain.diary.service;
 
+import com.korea.calender.domain.diary.entity.Diary;
 import com.korea.calender.domain.diary.repository.DiaryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class DiaryService {
 
     private final DiaryRepository diaryRepository;
+
+    public List<Diary> getList() {
+
+        return diaryRepository.findAll();
+    }
 }
